aeeefabd86e1fe2309574acec7cea7fb2e53986f
Christine Dodrill <xena@yolo-swag.com>
2015-05-06 08:28:06 -0700

    Call NimMain to init garbage collector

--- a.lua
+++ a.lua
@@ -2,9 +2,13 @@ local ffi = require "ffi"
 
 ffi.cdef [[
 int add5(int);
+void NimMain();
 ]]
 
 local a = ffi.load("./a.so")
+
+print "Calling NimMain"
+a.NimMain()
 
 print "calling a.add5(4)"
 print(a.add5(4))
