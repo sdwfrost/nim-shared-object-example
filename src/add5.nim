4ee1c1bdd101a6b90f53a06cf8f576584002a5a6
Christine Dodrill <xena@yolo-swag.com>
2015-05-06 21:14:07 -0700

    Add documentation to src/add5.nim

--- src/add5.nim
+++ src/add5.nim
@@ -1,2 +1,9 @@
-proc a*(b: int): int {.exportc: "add5"} =
+## This module provides a simple "add 5" function to test
+## Nim FFI with things that expect C functions.
+
+proc add5*(b: int): int {.exportc: "add5"} =
+  ## Returns its input plus 5
+  ##
+  ## A simple demonstration of a Nim proc that can be used
+  ## as a C function.
   b + 5
